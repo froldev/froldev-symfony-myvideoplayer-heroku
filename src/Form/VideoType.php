45aa54251b36baa0b23a30e564abfc3d7446f564
froldev <frol.developpement@gmail.com>
2021-02-17 21:54:50 +0100

    add wysiwyg in forms

--- src/Form/VideoType.php
+++ src/Form/VideoType.php
@@ -3,21 +3,29 @@
 namespace App\Form;
 
 use App\Entity\Video;
+use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class VideoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('url')
-            ->add('description')
-            ->add('link')
-            ->add('category')
-        ;
+            ->add('name', TextType::class, ['label' => 'Nom de la vidéo : '])
+            ->add('url', UrlType::class, ['label' => 'Lien de la vidéo : '])
+            ->add('description', CKEditorType::class, ['label' => 'Description : ', 'config' => ['toolbar' => 'standard']])
+            ->add('link', UrlType::class, ['label' => 'Lien de l\'auteur : '])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie'
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
