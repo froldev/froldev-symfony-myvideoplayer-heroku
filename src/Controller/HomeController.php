539c7d86db715bf51cccb5f533d4f7a5960f9d34
froldev <frol.developpement@gmail.com>
2021-02-28 12:30:43 +0100

    add best videos and paginator in template home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,24 +2,42 @@
 
 namespace App\Controller;
 
+use App\Form\SearchVideoType;
 use App\Repository\CategoryRepository;
 use App\Repository\UserRepository;
 use App\Repository\VideoRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class HomeController extends AbstractController
 {
     const MAX_ITEMS_NAV = 4;
+    const MAX_HOME_VIDEOS = 6;
 
     /**
      * @Route("/", name="home")
      */
-    public function index(CategoryRepository $categoryRepository): Response
-    {
+    public function index(
+        CategoryRepository $categoryRepository,
+        VideoRepository $videoRepository,
+        Request $request,
+        PaginatorInterface $paginator
+    ): Response {
+        $form = $this->createForm(SearchVideoType::class);
+
+        $videos = $paginator->paginate(
+            $videoRepository->findBy(['is_best' => true]),
+            $request->query->getInt('page', 1),
+            self::MAX_HOME_VIDEOS
+        );
+
         return $this->render('home/index.html.twig', [
             'categories' => $categoryRepository->findAll(),
+            'videos' => $videos,
+            'formVideo' => $form->createView(),
         ]);
     }
 
